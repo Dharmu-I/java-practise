e01cb51ddb098246583b2a27e16c1fd0dbb2254b
dharmu.i <dharmu.i@flipkart.com>
2020-02-18 10:09:42 +0530

    Chess moves

--- src/main/java/com/java/practise/dynamic_programming/ChessMoves.java
+++ src/main/java/com/java/practise/dynamic_programming/ChessMoves.java
@@ -1,31 +1,62 @@
 package com.java.practise.dynamic_programming;
 
+import java.util.Vector;
+
 public class ChessMoves {
 
-    public static void main(String[] args) {
-//        System.out.println(findMinChessMoves(0, 1,4,3));
-        System.out.println(findMinChessMovesDP(4,3));
+    static int sizeOfMatrix = 5;
+
+    static class Cell{
+        int x, y;
+        int dis;
+
+        public Cell(int x, int y, int dis)
+        {
+            this.x = x;
+            this.y = y;
+            this.dis = dis;
+        }
     }
+
+    public static void main(String[] args) {
+        System.out.println(findMinChessMoves(0, 1,4,3));
+      }
 
     private static int findMinChessMoves(int sourceM, int sourceN, int destinationM, int destinationN) {
-        System.out.println("S M "+sourceM+" S N "+sourceN+" D M "+destinationM+" D N "+destinationN);
-        if (sourceM == destinationM && sourceN == destinationN){
-            return 0;
+        int dx[] = {-2, -1, 1, 2, -2, -1, 1, 2};
+        int dy[] = {-1, -2, -2, -1, 1, 2, 2, 1};
+
+        Vector<Cell> queue = new Vector<>();
+        boolean[][] visited = new boolean[sizeOfMatrix][sizeOfMatrix];
+        for (int i = 0; i < sizeOfMatrix; i++)
+            for (int j = 0; j < sizeOfMatrix; j++)
+                visited[i][j] = false;
+
+        queue.add(new Cell(sourceM, sourceN, 0));
+        visited[sourceM][sourceN] = true;
+
+        while (!queue.isEmpty()){
+            Cell cell = queue.remove(0);
+
+            if (cell.x == destinationM && cell.y == destinationN){
+                return cell.dis;
+            }
+            for (int i = 0; i < dx.length; i++){
+                int x = dx[i] + cell.x;
+                int y = dy[i] + cell.y;
+                if (isInside(x, y) && !visited[x][y]){
+                    queue.add(new Cell(x, y, cell.dis+1));
+                    visited[x][y] = true;
+                }
+            }
         }
-        if (sourceM < 0 || sourceN < 0 || sourceM > 5 || sourceN > 5){
-            return Integer.MAX_VALUE;
-        }
-        return findMinChessMoves(sourceM+1,sourceN+2, destinationM, destinationN)+1;
+        return Integer.MAX_VALUE;
     }
 
-    static int[][] board = new int[5][5];
-
-    private static int findMinChessMovesDP(int destinationM, int destinationN){
-        int delta = destinationM - destinationN;
-
-        if( destinationN > delta )
-            return 2 * ( ( destinationN - delta ) / 3 ) + delta;
-        else
-            return delta - 2 * ( ( delta - destinationN ) / 4 );
+    private static boolean isInside(int x, int y){
+        if (x >=0 && y>= 0 && x < sizeOfMatrix && y < sizeOfMatrix){
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
     }
 }
