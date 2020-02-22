387d4c67d296dda74486d0e170bf35b6f9a6fe0d
dharmu.i <dharmu.i@flipkart.com>
2020-02-22 16:46:27 +0530

    Builder pattern

---
+++ src/main/java/com/java/practise/design/builder/InvoiceBuilder.java
@@ -0,0 +1,13 @@
+package com.java.practise.design.builder;
+
+/**
+ * dharmu.i on 13/03/19
+ */
+
+public interface InvoiceBuilder {
+
+    void buildTotalAmount();
+    void buildItem();
+    void buildCustomerName();
+    void buildInvoiceType();
+}
---
+++ src/main/java/com/java/practise/design/builder/InvoiceContent.java
@@ -0,0 +1,12 @@
+package com.java.practise.design.builder;
+
+/**
+ * dharmu.i on 13/03/19
+ */
+
+public interface InvoiceContent {
+    Integer setTotalAmount();
+    String setInvoiceType();
+    String setCustomerName();
+    String setItem();
+}
