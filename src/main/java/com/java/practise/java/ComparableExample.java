0a98902250b8f35d9b54cb2d9268fe1077f81856
dharmu.i <dharmu.i@flipkart.com>
2020-03-18 21:36:40 +0530

    comparable example

---
+++ src/main/java/com/java/practise/java/ComparableExample.java
@@ -0,0 +1,41 @@
+package com.java.practise.java;
+
+import java.util.*;
+
+public class ComparableExample {
+
+    private static void display(List<Person> personList){
+        for (Person person : personList){
+            System.out.println("Name : "+person.name+"\t Age : "+person.age);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Person> personList = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            personList.add(new Person(new Random().nextInt(99), "Person"+i));
+        }
+        System.out.println("===Before sort====");
+        display(personList);
+        System.out.println("===After sort====");
+        Collections.sort(personList);
+        display(personList);
+    }
+}
+
+class Person implements Comparable<Person>{
+
+    int age;
+
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.age - o.age;
+    }
+}
